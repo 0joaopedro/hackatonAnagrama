b6700d4d591cd6b0dae2ae21a45c993ddd5b13bc
joao <jopedoss@gmail.com>
2019-11-29 18:20:34 -0300

    melhor entendimento

--- anagrama.py
+++ anagrama.py
@@ -1,65 +1,63 @@
-MIN_WORD_SIZE = 2 # tamanho mínimo da saída
-
 class Node(object):
-    def __init__(self, letter='', final=False, depth=0):
-        self.letter = letter
+    def __init__(self, letra='', final=False, tamanho=0):
+        self.letra = letra
         self.final = final
-        self.depth = depth
+        self.tamanho = tamanho
         self.children = {}
-    def add(self, letters):
+    def add(self, letras):
         node = self
-        for index, letter in enumerate(letters):
-            if letter not in node.children:
-                node.children[letter] = Node(letter, index==len(letters)-1, index+1)
-            node = node.children[letter]
-    def anagram(self, letters):
-        tiles = {}
-        for letter in letters:
-            tiles[letter] = tiles.get(letter, 0) + 1
-        min_length = len(letters)
-        return self._anagram(tiles, [], self, min_length)
-    def _anagram(self, tiles, path, root, min_length):
-        if self.final and self.depth >= MIN_WORD_SIZE:
-            word = ''.join(path)
-            length = len(word.replace(' ', ''))
-            if length >= min_length:
-                yield word
+        for index, letra in enumerate(letras):
+            if letra not in node.children:
+                node.children[letra] = Node(letra, index==len(letras)-1, index+1)
+            node = node.children[letra]
+    def anagrama(self, letras):
+        escopo = {}
+        for letra in letras:
+            escopo[letra] = escopo.get(letra, 0) + 1
+        tamanho_min = len(letras)
+        return self._anagrama(escopo, [], self, tamanho_min)
+    def _anagrama(self, escopo, path, root, tamanho_min):
+        if self.final:
+            palavra = ''.join(path)
+            length = len(palavra.replace(' ', ''))
+            if length >= tamanho_min:
+                yield palavra
             path.append(' ')
-            for word in root._anagram(tiles, path, root, min_length):
-                yield word
+            for palavra in root._anagrama(escopo, path, root, tamanho_min):
+                yield palavra
             path.pop()
-        for letter, node in self.children.items():
-            count = tiles.get(letter, 0)
-            if count == 0:
+        for letra, node in self.children.items():
+            c = escopo.get(letra, 0)
+            if c == 0:
                 continue
-            tiles[letter] = count - 1
-            path.append(letter)
-            for word in node._anagram(tiles, path, root, min_length):
-                yield word
+            escopo[letra] = c - 1
+            path.append(letra)
+            for palavra in node._anagrama(escopo, path, root, tamanho_min):
+                yield palavra
             path.pop()
-            tiles[letter] = count
+            escopo[letra] = c
 
-def load_dictionary(path):
-    result = Node()
-    for line in open(path, 'r'):
-        word = line.strip().lower()
-        result.add(word)
-    return result
+def carrega_arquivo(path):
+    retorno = Node()
+    for linha in open(path, 'r'):
+        palavra = linha.strip().lower()
+        retorno.add(palavra)
+    return retorno
 
 def main():
     print ('Carregando lista.')
-    words = load_dictionary('palavras.txt')
+    palavras = carrega_arquivo('palavras.txt')
     while True:
-        letters = input('Digite as palavras: ')
-        letters = letters.lower()
-        letters = letters.replace(' ', '')
-        if not letters:
+        letras = input('Digite as palavras: ')
+        letras = letras.lower()
+        letras = letras.replace(' ', '')
+        if not letras:
             break
-        count = 0
-        for word in words.anagram(letters):
-            print (word)
-            count += 1
-        print(('%d resultados.' % count))
+        c = 0
+        for palavra in palavras.anagrama(letras):
+            print (palavra)
+            c += 1
+        print(('%d resultados.' % c))
 
 if __name__ == '__main__':
     main()
